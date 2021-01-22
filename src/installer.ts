eb8dddf4f59d87982c67a345a0875309750fd5a0
mbui <Minhtri.bui@md.de>
2021-01-22 11:19:35 +0100

    fiele updated

--- src/installer.ts
+++ src/installer.ts
@@ -3,6 +3,6 @@ import * as tc from '@actions/tool-cache'
 
 export async function get(version: string): Promise<string> {
   const toolPath = tc.find('ant', version)
-  console.log('tool path  ${toolPath}')
+  core.debug('tool path  ${toolPath}')
   return toolPath
 }
