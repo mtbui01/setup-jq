118349f01dc61f252bf6b176accc15c124907b00
mbui <Minhtri.bui@md.de>
2021-01-23 11:21:29 +0100

    [release]

--- src/installer.ts
+++ src/installer.ts
@@ -2,6 +2,7 @@ import * as core from '@actions/core'
 import * as tc from '@actions/tool-cache'
 
 export async function get(version: string): Promise<string> {
+	
   const toolPath = tc.find('ant', version)
   core.debug('tool path  ${toolPath}')
   return toolPath
