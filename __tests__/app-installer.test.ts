9c1314ab2dab9b0b3c714e1f2f17f21c24de4229
mbui <Minhtri.bui@md.de>
2021-01-24 14:53:54 +0100

    new module utils added

--- __tests__/app-installer.test.ts
+++ __tests__/app-installer.test.ts
@@ -1,4 +1,4 @@
-import * as installer from '../src/installer'
+import * as installer from '../src/app-installer'
 import * as process from 'process'
 import * as cp from 'child_process'
 import * as path from 'path'
