2e17ee60b5c5bc3a784f22ec73f39b819464584c
mbui <Minhtri.bui@md.de>
2021-01-29 07:30:21 +0100

    unused log removed

--- __tests__/global-setup.js
+++ __tests__/global-setup.js
@@ -1,7 +1,6 @@
 const io = require('@actions/io')
 const path = require('path')
 module.exports = async () => {
-  console.log('setup test')
   const toolDir = path.join(__dirname, 'runner', 'tools')
   const tempDir = path.join(__dirname, 'runner', 'temp')
 
@@ -9,8 +8,6 @@ module.exports = async () => {
 
   process.env['RUNNER_TEMP'] = tempDir
   
-  console.log(`clear tooldir:${toolDir} and tempDir: ${tempDir}`)
-
   await io.rmRF(toolDir)
   await io.rmRF(tempDir)
 }
